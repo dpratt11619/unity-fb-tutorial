b59e78c41a176c5e2595b3447fc40392250b38c4
Daniel Pratt <danielpratt11@gmail.com>
2017-01-29 19:31:25 +0000

    Initial commit.

---
+++ Flappy Bird Unity Tutorial/Assets/Scripts/Bird.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	public float upForce = 100f;
+
+	private bool isDead = false;
+
+	private Rigidbody2D rb2d;
+
+	private Animator anim;
+
+	const string flap = "Flap";
+
+	const string die = "Die";
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isDead) {
+			if (Input.GetMouseButton (0)) {
+				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce(new Vector2(0, upForce));
+				anim.SetTrigger (flap);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D () {
+		rb2d.velocity = Vector2.zero;
+		isDead = true;
+		anim.SetTrigger (die);
+		GameControl.instance.BirdDied ();
+	}
+
+
+}
---
+++ Flappy Bird Unity Tutorial/Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnPool : MonoBehaviour {
+
+	public int columnPoolSize = 5;
+
+	public GameObject columnPrefab;
+
+	public float spawnRate = 4f;
+
+	private GameObject[] columns;
+
+	private Vector2 objectPoolPosition = new Vector2 (-15f, -25f);
+
+	private float timeSinceLastSpawned;
+
+	// Use this for initialization
+	void Start () {
+		columns = new GameObject[columnPoolSize];
+
+		for (int i = 0; i < columnPoolSize; i++) {
+			columns [i] = (GameObject)Instantiate (columnPrefab, objectPoolPosition, Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastSpawned += Time.deltaTime;
+
+		if (!GameControl.instance.gameOver && timeSinceLastSpawned >= spawnRate) {
+			
+		}
+	}
+}
