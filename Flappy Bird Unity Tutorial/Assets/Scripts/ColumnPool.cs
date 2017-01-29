ea62872bffdea14fc9426538a4b05f1708d4fae5
Daniel Pratt <danielpratt11@gmail.com>
2017-01-29 19:43:26 +0000

    Finished.

--- Flappy Bird Unity Tutorial/Assets/Scripts/ColumnPool.cs
+++ Flappy Bird Unity Tutorial/Assets/Scripts/ColumnPool.cs
@@ -10,11 +10,19 @@ public class ColumnPool : MonoBehaviour {
 
 	public float spawnRate = 4f;
 
+	public float columnMin = -1f;
+
+	public float columnMax = 3.5f;
+
 	private GameObject[] columns;
 
 	private Vector2 objectPoolPosition = new Vector2 (-15f, -25f);
 
 	private float timeSinceLastSpawned;
+
+	private float spawnXPosition = 10f;
+
+	private int currentColumn = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -30,7 +38,13 @@ public class ColumnPool : MonoBehaviour {
 		timeSinceLastSpawned += Time.deltaTime;
 
 		if (!GameControl.instance.gameOver && timeSinceLastSpawned >= spawnRate) {
-			
+			timeSinceLastSpawned = 0;
+			float spawnYPosition = Random.Range (columnMin, columnMax);
+			columns [currentColumn].transform.position = new Vector2 (spawnXPosition, spawnYPosition);
+			currentColumn++;
+			if (currentColumn >= columnPoolSize) {
+				currentColumn = 0;
+			}
 		}
 	}
 }
